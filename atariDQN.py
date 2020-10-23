81a3f839114f5083386be13639dae5b98e1bd44f
Sjtugjl <46067967+Sjtugjl@users.noreply.github.com>
2020-10-23 22:20:26 +0800

    skip_steps

--- atariDQN.py
+++ atariDQN.py
@@ -17,16 +17,16 @@ from gym import wrappers
 from utils import *
 from queue import PriorityQueue
 
-# hyper-parameter.  
+# hyper-parameter.
 EPISODES = 500
-CYF = 1#delete when handing in.
+CYF = 1  # delete when handing in.
 use_gpu = True
-
+act_rpt = 5
 
 class DQNAgent:
     def __init__(self, state_size, action_size):
         # if you want to see MsPacman learning, then change to True
-        self.render = False
+        self.render = True
 
         # get size of state and action
         self.state_size = state_size
@@ -45,9 +45,10 @@ class DQNAgent:
         self.UPDATE_TARGET_EVERY = 5
 
         # create replay memory using deque
-        self.maxlen = 8000 
+        self.maxlen = 8000
         self.memory = deque(maxlen=self.maxlen)
-        #self.priority_memory = PriorityQueue(maxsize=self.maxlen)
+        self.preprocess_stack = deque([], maxlen=2)
+        # self.priority_memory = PriorityQueue(maxsize=self.maxlen)
         self.memory_list = []
 
         # create main model
@@ -71,7 +72,7 @@ class DQNAgent:
 
     # get action from model using epsilon-greedy policy
     def get_action(self, state):
-        if np.random.rand() <= self.epsilon:   # choose action randomly
+        if np.random.rand() <= self.epsilon:  # choose action randomly
             # Decay Epsilon
             agent.epsilon_decay()
             return random.randrange(self.action_size)
@@ -81,21 +82,19 @@ class DQNAgent:
             q_value = self.model_eval.predict(state)
             return np.argmax(q_value[0])
 
-    def choose_reward(self,i):
+    def choose_reward(self, i):
         return i[2]
-
 
     # save sample <s,a,r,s'> to the replay memory
     def append_sample(self, state, action, reward, next_state, done):
-        #self.memory.append((state, action, reward, next_state, done))
-        #self.priority_memory.put((-1*reward, action, state, next_state, done))
+        # self.memory.append((state, action, reward, next_state, done))
+        # self.priority_memory.put((-1*reward, action, state, next_state, done))
         if len(self.memory_list) < self.maxlen:
             self.memory_list.append((state, action, reward, next_state, done))
-            self.memory_list.sort(key=self.choose_reward,reverse=True)
+            self.memory_list.sort(key=self.choose_reward, reverse=True)
         else:
-            self.memory_list[self.maxlen-1] = (state, action, reward, next_state, done)
-            self.memory_list.sort(key=self.choose_reward,reverse=True)
-
+            self.memory_list[self.maxlen - 1] = (state, action, reward, next_state, done)
+            self.memory_list.sort(key=self.choose_reward, reverse=True)
 
     def epsilon_decay(self):
         if len(self.memory) < self.train_start:
@@ -103,7 +102,7 @@ class DQNAgent:
         # epsilon decay.
         if self.epsilon > self.EPS_END:
             self.epsilon = self.EPS_END + (self.EPS_START - self.EPS_END) * \
-            math.exp(-1. * self.step_done / self.EPS_DECAY)
+                           math.exp(-1. * self.step_done / self.EPS_DECAY)
         self.step_done += 1
 
     # pick samples randomly from replay memory (with batch_size)
@@ -113,7 +112,7 @@ class DQNAgent:
         batch_size = min(self.batch_size, len(self.memory))
         mini_batch = self.memory[0:batch_size]
 
-        #mini_batch = random.sample(self.memory, batch_size)
+        # mini_batch = random.sample(self.memory, batch_size)
 
         update_input = np.zeros((batch_size, self.state_size))
         update_target = np.zeros((batch_size, self.state_size))
@@ -139,16 +138,17 @@ class DQNAgent:
 
         # and do the model fit!
         self.model_eval.fit(update_input, target, batch_size=self.batch_size,
-                       epochs=1, verbose=0)
-    
+                            epochs=1, verbose=0)
+
     def eval2target(self):
         self.model_target.set_weights(self.model_eval.get_weights())
+
 
 if __name__ == "__main__":
     # load the gym env
     env = gym.make('MsPacman-ram-v0')
     # set  random seeds to get reproduceable result(recommended)
-    set_random_seed(0,cyf=CYF)
+    set_random_seed(0)
     # get size of state and action from environment
     state_size = env.observation_space.shape[0]
     action_size = env.action_space.n
@@ -160,6 +160,7 @@ if __name__ == "__main__":
     graph_score = []
     avg_length = 10
     sum_score = 0
+    total_score = 0
     start = time.time()
     end = time.time()
     # train DQN
@@ -171,19 +172,24 @@ if __name__ == "__main__":
         state = env.reset()
         state = np.reshape(state, [1, state_size])
         lives = 3
-        while not done: 
-            dead = False         
+        while not done:
+            dead = False
             while not dead:
+                reward = 0
                 # render the gym env
                 if agent.render:
                     env.render()
                 # get action for the current state
                 action = agent.get_action(state)
                 # take the action in the gym env, obtain the next state
-                next_state, reward, done, info = env.step(action)
-                next_state = np.reshape(next_state, [1, state_size])
+                # skip frames by repeating action
+                for i in range(act_rpt):
+                    next_state, _reward, done, info = env.step(action)
+                    reward += _reward
+                    next_state = np.reshape(next_state, [1, state_size])
+                    agent.preprocess_stack.append(next_state)
                 # judge if the agent dead
-                dead = info['ale.lives']<lives
+                dead = info['ale.lives'] < lives
                 lives = info['ale.lives']
                 # update score value
                 score += reward
@@ -198,7 +204,7 @@ if __name__ == "__main__":
                 state = next_state
 
             # update the target network after some iterations.
-            if e%agent.UPDATE_TARGET_EVERY == 0:
+            if e % agent.UPDATE_TARGET_EVERY == 0:
                 agent.eval2target()
 
         # print info and draw the figure.
@@ -206,21 +212,23 @@ if __name__ == "__main__":
             end = time.time()
             scores.append(score)
             sum_score += score
+            total_score += score
             episodes.append(e)
 
             # plot the reward each episode
             # pylab.plot(episodes, scores, 'b')
 
             print("\nscore:", score, "  memory length:",
-            len(agent.memory), "  epsilon:", agent.epsilon)
-        if e%avg_length == 0:
+                  len(agent.memory), "  epsilon:", agent.epsilon, "average score :", float(total_score/(e+1)))
+        if e % avg_length == 0:
             graph_episodes.append(e)
             graph_score.append(sum_score / avg_length)
-            print(sum_score/avg_length)
+            print(sum_score / avg_length)
             sum_score = 0
+
             # plot the reward each avg_length episodes
             pylab.plot(graph_episodes, graph_score, 'r')
             pylab.savefig("./pacman_avg.png")
-        
+
         # save the network if you want to test it.
-       #agent.model_target.save('model_{0}.h5'.format(e))
+    # agent.model_target.save('model_{0}.h5'.format(e))
